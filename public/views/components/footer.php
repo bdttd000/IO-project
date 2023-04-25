3db8a9d7ea82daa62cd2dd8f1e7d66a3b711c85e
Kuba <kuba428@op.pl>
2023-04-25 15:09:23 +0200

    added includes to pages

---
+++ public/views/components/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container footer-content">
+        <ul class="footer-list">
+            <li>Regulamin</li>
+            <li>|</li>
+            <li>Kontakt</li>
+            <li>|</li>
+            <li>Polityka prwatności</li>
+        </ul>
+    </div>
+</footer>
