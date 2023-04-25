b23d5cb5a55a0b809f45ffdcc1e0e249c514bb3c
bdttd000 <44643460+bdttd000@users.noreply.github.com>
2023-04-25 14:43:06 +0200

    Feature/adding login validation (#15)
    * php login verification with sessions
    
    * moving redirect to main appController
    
    * added request controller

---
+++ public/views/login.php
@@ -0,0 +1,34 @@
+<?php
+$SessionController = new SessionController;
+if ($SessionController::isLogged() === "true") {
+    $SessionController->redirectToHome();
+}
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        if (isset($messages)) {
+            echo $messages['error'];
+        }
+        ?>
+    </div>
+    <form action="checkLogin" method="POST">
+        <input type="text" name="email" placeholder="Wprowadź email" value="<?php if (isset($messages['email'])) {
+            echo $messages['email'];
+        } ?>">
+        <input type="password" name="password" placeholder="Wprowadź hasło">
+        <button type="submit">Zaloguj</button>
+    </form>
+</body>
+
+</html>
---
+++ public/views/mainPage.php
@@ -0,0 +1,31 @@
+<?php
+$SessionController = new SessionController;
+$userIsAuthenticated = $SessionController::isLogged() === "true";
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if ($userIsAuthenticated) {
+        echo '<div>Hejka</div>';
+    } else {
+        echo '<div>Nie wiem kim jesteś</div>';
+    }
+
+    if ($userIsAuthenticated) {
+        echo '<a href="logout">Wyloguj</a>';
+    } else {
+        echo '<a href="login">Logowanie</a>';
+    }
+    ?>
+</body>
+
+</html>
