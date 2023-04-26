1d05bac40e2da7f4d8067cf1800cad8531e1f08f
Kuba <kuba428@op.pl>
2023-04-26 20:15:20 +0200

    added favicon to views

--- public/views/home.php
+++ public/views/home.php
@@ -10,7 +10,8 @@ $userIsAuthenticated = $SessionController::isLogged() === "true";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
-    <title>Document</title>
+    <link rel="icon" href="public/img/logo.svg" type="image/svg+xml">
+    <title>Główna</title>
 </head>
 
 <body>
