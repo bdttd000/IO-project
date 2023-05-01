bff431b74d1b95637d4de05028b9d0dd862e722c
bdttd000 <44643460+bdttd000@users.noreply.github.com>
2023-05-01 19:45:52 +0000

    Revert "Feature/footer links (#32)"
    This reverts commit d42a4d5357085dce4490efecae5f4e2e537dce63.

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
 
-require 'Routing.php';
-
-$path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url($path, PHP_URL_PATH);
-
-Routing::get('', 'DefaultController');
-Routing::get('home', 'DefaultController');
-Routing::get('login', 'DefaultController');
-Routing::get('register', 'DefaultController');
-Routing::get('statute', 'DefaultController');
-Routing::get('contact', 'DefaultController');
-Routing::get('privacyPolicy', 'DefaultController');
-
-Routing::post('checkLogin', 'SecurityController');
-
-Routing::post('logout', 'SessionController');
-
-Routing::run($path);
+echo "Hi there";
