a34a598a298cf26f690f9326b59ba0dbbb813ed5
Kuba <kuba428@op.pl>
2023-05-08 01:44:59 +0200

    added headImports component and included it

--- public/views/contact.php
+++ public/views/contact.php
@@ -6,13 +6,7 @@ $userIsAuthenticated = $SessionController::isLogged() === "true";
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="icon" href="public/img/logo.svg" type="image/svg+xml">
-    <script src="public/js/sidebar-interactions.js" defer></script>
-    <script src="public/js/navbar-interactions.js" defer></script>
+    <?php include("public/views/components/headImports.php"); ?>
     <title>Kontakt</title>
 </head>
 
