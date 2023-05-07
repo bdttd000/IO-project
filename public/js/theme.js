18d7aeb2622147f0a58f2598519dc2ef9835651f
Kuba <kuba428@op.pl>
2023-05-08 01:41:31 +0200

    added theme logic for local storage

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,12 +1,23 @@
+let activeTheme = localStorage.getItem("theme") ?? "dark";
+
 var theme = {
-    dark: {
+    "dark": {
         "--main-bg": "var(--grey-900)",
         "--navbar-footer-bg": "var(--grey-1000)",
         "--card-bg": "var(--grey-100)",
     },
-    light: {
+    "light": {
         "--main-bg": "var(--grey-300)",
         "--navbar-footer-bg": "var(--grey-100)",
         "--card-bg": "var(--grey-100)",
+    }
+}
+
+if (activeTheme !== "dark") {
+    for (const [themeName, rootConfig] of Object.entries(theme)) {
+        if (themeName !== activeTheme) continue; 
+        for (const [className, value] of Object.entries(rootConfig)) {
+            document.querySelector(':root').style.setProperty(className, value);
+        }
     }
 }
