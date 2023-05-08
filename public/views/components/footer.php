a4b99d4349fde054ece7b85dc115aef6613b13cc
Kuba <kuba428@op.pl>
2023-05-08 03:42:30 +0200

    added footer toggle button

--- public/views/components/footer.php
+++ public/views/components/footer.php
@@ -1,6 +1,8 @@
 <footer class="footer">
     <div class="container footer-content">
         <ul class="footer-list">
+            <li class="footer-lx-only" onclick="listenToggleText()">Zmień motyw</li>
+            <li class="footer-lx-only">|</li>
             <li><a href="statute">Regulamin</a></li>
             <li>|</li>
             <li><a href="contact">Kontakt</a></li>
