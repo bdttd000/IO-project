690be5091dd23e0b86e8a751cbbfdcef650f45b2
Kuba <kuba428@op.pl>
2023-05-08 03:42:49 +0200

    added toggle button logic

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,8 +1,11 @@
 let activeTheme = localStorage.getItem("theme") ?? "dark";
+const toggleButton = document.getElementById("toggleButton");
+const toggleCheckbox = document.getElementById("toggleCheckbox");
+
 
 var theme = {
     "dark": {
-        "--main-bg": "var(--grey-900)",
+        "--main-bg": "var(--grey-800)",
         "--navbar-footer-bg": "var(--grey-1000)",
         "--card-bg": "var(--grey-100)",
     },
@@ -13,7 +16,7 @@ var theme = {
     }
 }
 
-if (activeTheme !== "dark") {
+const applyTheme = () => {
     for (const [themeName, rootConfig] of Object.entries(theme)) {
         if (themeName !== activeTheme) continue; 
         for (const [className, value] of Object.entries(rootConfig)) {
@@ -21,3 +24,18 @@ if (activeTheme !== "dark") {
         }
     }
 }
+
+applyTheme();
+
+const listenToggle = () => {
+    activeTheme = activeTheme === "dark" ? "light" : "dark";
+    localStorage.setItem("theme", activeTheme);
+    applyTheme();
+}
+
+const listenToggleText = () => {
+    toggleCheckbox.checked = !toggleCheckbox.checked;
+    listenToggle();
+}
+
+toggleButton.addEventListener("click", listenToggle)
