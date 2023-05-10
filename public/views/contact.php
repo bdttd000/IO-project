caecf07cea76fb993a46f66e262b9e3598d0f4b4
MnStan <58117854+MnStan@users.noreply.github.com>
2023-05-10 15:07:37 +0200

    Added Contact page (#43)
    * Added Contact page
    
    * Update public/css/components/textarea.css
    
    Co-authored-by: bdttd000 <44643460+bdttd000@users.noreply.github.com>
    
    * Update public/css/components/textarea.css
    
    Co-authored-by: bdttd000 <44643460+bdttd000@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: bdttd000 <44643460+bdttd000@users.noreply.github.com>

--- public/views/contact.php
+++ public/views/contact.php
@@ -1,6 +1,59 @@
 <?php
 $SessionController = new SessionController;
 $userIsAuthenticated = $SessionController::isLogged() === "true";
+
+require_once "public/views/components/card.php";
+require_once "public/views/components/input.php";
+require_once "public/views/components/form.php";
+require_once "public/views/components/button.php";
+require_once "public/views/components/textarea.php";
+require_once "public/views/contents/contactContent.php";
+
+$inputName = [
+    'type' => 'name',
+    'name' => 'Imię',
+    'placeholder' => "Wprowadź imię",
+    'value' => $messages['name'],
+];
+
+$inputSurname = [
+    'type' => 'name',
+    'name' => 'nazwisko',
+    'placeholder' => "Wprowadź nazwisko",
+    'value' => $messages['surname'],
+];
+
+$inputEmail = [
+    'type' => 'name',
+    'name' => 'email',
+    'placeholder' => "Wprowadź adres email",
+    'value' => $messages['email'],
+];
+
+$inputMessage = [
+    'name' => 'message',
+    'placeholder' => "Opisz swój problem",
+    'value' => $messages['message'],
+];
+
+$sendButton = [
+    'type' => 'submit',
+    'value' => 'Wyślij',
+];
+
+$formContent = [
+    'action' => 'sendContact',
+    'method' => 'POST',
+    'content' => $contactContent . Input($inputName) . Input($inputSurname) . Input($inputEmail) . Textarea($inputMessage) . Button($sendButton)
+];
+
+$cardContent = Form($formContent);
+
+$cardArray = [
+    'title' => "Kontakt",
+    'content' => $cardContent
+]
+
 ?>
 
 <html lang="en">
@@ -13,7 +66,9 @@ $userIsAuthenticated = $SessionController::isLogged() === "true";
 <body>
     <?php include("public/views/components/navbar.php"); ?>
     <?php include("public/views/components/sidebar.php"); ?>
-    <div class="body-container">contact</div>
+    <main class="conainter flex flex-center flex-column">
+        <?php echo Card($cardArray); ?>
+    </main>
     <?php include("public/views/components/footer.php"); ?>
 </body>
 
