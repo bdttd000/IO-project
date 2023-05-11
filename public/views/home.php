b3be57576e6f4edc509d07f6e2ae3ec07fbe89db
Kuba <kuba428@op.pl>
2023-05-11 16:22:22 +0200

    removed home page meme checker

--- public/views/home.php
+++ public/views/home.php
@@ -14,10 +14,6 @@ $userIsAuthenticated = $SessionController::isLogged() === "true";
     <?php include("public/views/components/navbar.php"); ?>
     <?php include("public/views/components/sidebar.php"); ?>
     <main class="container flex flex-center flex-column">
-        <h2>
-            <?= $meme->getTitle() ?>
-        </h2>
-        <img src="public/uploads/<?= $meme->getImage() ?>" alt="">
     </main>
     <?php include("public/views/components/footer.php"); ?>
 </body>
