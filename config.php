fccf3393917c532d331cd3e4d6e7c84a2039e54c
MnStan <mmaks97@gmail.com>
2023-05-14 17:43:04 +0200

    Added meme component

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+const USERNAME = 'postgres';
+const PASSWORD = '5lhKukap0WJwTWmJDyBM';
+const HOST = 'containers-us-west-199.railway.app';
+const DATABASE = 'railway';
+const PORT = '7624';
