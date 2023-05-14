00006c46323e9366b42eaea4382e600b7446cea8
MnStan <mmaks97@gmail.com>
2023-05-14 17:53:55 +0200

    Revert "Added meme component"
    This reverts commit fccf3393917c532d331cd3e4d6e7c84a2039e54c.

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const USERNAME = 'postgres';
-const PASSWORD = '5lhKukap0WJwTWmJDyBM';
-const HOST = 'containers-us-west-199.railway.app';
-const DATABASE = 'railway';
-const PORT = '7624';
--- public/views/home.php
+++ public/views/home.php
@@ -1,31 +1,6 @@
 <?php
 $SessionController = new SessionController;
 $userIsAuthenticated = $SessionController::isLogged() === "true";
-
-@require_once "public/views/components/meme.php";
-@require_once "public/views/components/button.php";
-@require_once "public/views/components/memeComment.php";
-
-$buttonArray = [
-    'type' => 'more',
-    'value' => 'Zobacz więcej / Dodaj komentarz'
-];
-
-$commentsArray = [
-    'comment1' => 'Bardzo fajny memik, pozdrawiam z rodzinka. Pozdrów mame i tate :))))',
-    'comment2' => 'Bardzo fajny memik, pozdrawiam z rodzinka. Pozdrów mame i tate :))))',
-    'comment3' => 'Bardzo fajny memik, pozdrawiam z rodzinka. Pozdrów mame i tate :))))',
-];
-
-$memeArray = [
-    'title' => 'Testtestowanie',
-    'likes' => '999',
-    'username' => 'Username',
-    'meme-date' => '17.04.2023',
-    'comments' => $commentsArray,
-    'comment' => 'Bardzo fajny memik, pozdrawiam z rodzinka. Pozdrów mame i tate :))))',
-    'button' => Button($buttonArray)
-]
 ?>
 
 <html lang="en">
@@ -38,14 +13,11 @@ $memeArray = [
 <body>
     <?php include("public/views/components/navbar.php"); ?>
     <?php include("public/views/components/sidebar.php"); ?>
-    <main class="container flex flex-center flex-row" style="gap: 2rem">
-        <section style="width: 70%; height: 100px; gap: 20px;" class="flex flex-center flex-column">
-            <?php echo Meme($memeArray) ?>
-            <?php echo Meme($memeArray) ?>
-            <?php echo Meme($memeArray) ?>
-            <?php echo Meme($memeArray) ?>
-        </section>
-        <aside style="width: 30%; height: 100px;"></aside>
+    <main class="container flex flex-center flex-column">
+        <h2>
+            <?= $meme->getTitle() ?>
+        </h2>
+        <img src="public/uploads/<?= $meme->getImage() ?>" alt="">
     </main>
     <?php include("public/views/components/footer.php"); ?>
 </body>
