bdd478537a7cfe16a84d09d0aee65fd9b97c6a12
bdttd000 <44643460+bdttd000@users.noreply.github.com>
2023-05-20 18:39:16 +0200

    Feature/meme model (#59)
    * added getNextId in main repository
    
    * added meme model

--- src/repository/Repository.php
+++ src/repository/Repository.php
@@ -10,4 +10,16 @@ class Repository
     {
         $this->database = new Database();
     }
+
+    public function getNextId($table_name, $cell_name): int
+    {
+        $stmt = $this->database->connect()->prepare("
+            SELECT max(" . $cell_name . ") FROM " . $table_name . "
+        ");
+        $stmt->execute();
+
+        $output = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $output['max'] + 1 ?: 1;
+    }
 }
