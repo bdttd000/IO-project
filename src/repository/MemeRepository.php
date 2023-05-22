aceb1a178d76a907f45068ef60534f4267dda43b
bdttd000 <44643460+bdttd000@users.noreply.github.com>
2023-05-22 21:46:44 +0200

    Feature/meme repository get meme (#62)
    * get meme logic
    
    * home changes to check function

--- src/repository/MemeRepository.php
+++ src/repository/MemeRepository.php
@@ -26,4 +26,57 @@ class MemeRepository extends Repository
             $evaluationDate
         ]);
     }
+
+    public function getMemes(int $pageNumber, int $numberOfMemes, bool $onlyEvaluated, int $userid = 0)
+    {
+        $offset = ($pageNumber - 1) * $numberOfMemes;
+
+        if ($userid) {
+            $stmt = $this->database->connect()->prepare('
+            SELECT * FROM meme_main WHERE userid = :userid ORDER BY memeid OFFSET :offset ROWS FETCH NEXT :numberofmemes ROWS ONLY
+            ');
+            $stmt->bindParam(':userid', $userid, PDO::PARAM_INT);
+        } else {
+            $stmt = $this->database->connect()->prepare('
+            SELECT * FROM meme_main ORDER BY memeid OFFSET :offset ROWS FETCH NEXT :numberofmemes ROWS ONLY
+            ');
+        }
+
+        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+        $stmt->bindParam(':numberofmemes', $numberOfMemes, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $memes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $result = [];
+
+        foreach ($memes as $meme) {
+            array_push($result, new Meme(...array_values($meme)));
+        }
+
+        return $result;
+    }
+
+    public function getMeme(int $id): ?Meme
+    {
+        $stmt = $this->database->connect()->prepare('
+            SELECT * FROM meme_main WHERE memeid = :id
+        ');
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $meme = array_values($stmt->fetch(PDO::FETCH_ASSOC));
+
+        return new Meme(...$meme);
+    }
+
+    public function printMeme(Meme $meme): void
+    {
+        echo $meme->getMemeID() . ' '
+            . $meme->getUserID() . ' '
+            . $meme->getTitle() . ' '
+            . $meme->getPhotoUrl() . ' '
+            . $meme->getCreationDate() . ' '
+            . $meme->getEvaluated() . ' '
+            . $meme->getEvaluationDate();
+    }
 }
