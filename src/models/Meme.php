edcb97fbbb5daf51967ca57de4a51b69bd2b6008
Jakub Wartak <kuba428@op.pl>
2023-05-23 18:06:13 +0200

    meme model adjustment

--- src/models/Meme.php
+++ src/models/Meme.php
@@ -9,6 +9,9 @@ class Meme
     private $creationDate;
     private $evaluated;
     private $evaluationdate;
+    private $comments;
+    private $likes;
+    private $followed;
 
     public function __construct(
         int $memeid,
@@ -17,7 +20,10 @@ class Meme
         string $photourl,
         string $creationDate,
         string $evaluated,
-        string $evaluationdate
+        string $evaluationdate,
+        array $comments,
+        int $likes,
+        int $followed
     ) {
         $this->memeid = $memeid;
         $this->userid = $userid;
@@ -26,6 +32,9 @@ class Meme
         $this->creationDate = $creationDate;
         $this->evaluated = $evaluated;
         $this->evaluationdate = $evaluationdate;
+        $this->comments = $comments;
+        $this->likes = $likes;
+        $this->followed = $followed;
     }
 
     public function getMemeID(): int
@@ -61,5 +70,25 @@ class Meme
     public function getEvaluationDate(): string
     {
         return $this->evaluationdate;
+    }
+
+    public function setComments($comments): void
+    {
+        $this->comments = $comments;
+    }
+
+    public function getComments(): array
+    {
+        return $this->comments;
+    }
+
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    public function getFollowed(): int
+    {
+        return $this->followed;
     }
 }
