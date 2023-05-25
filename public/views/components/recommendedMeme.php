6dcd3750a072a0e1090f2f859e74648bcafb961b
Jakub Wartak <kuba428@op.pl>
2023-05-25 19:56:33 +0200

    recommended meme changes

--- public/views/components/recommendedMeme.php
+++ public/views/components/recommendedMeme.php
@@ -1,14 +1,43 @@
 <?php
 
-function RecommendedMeme($content): string 
-{
-    $output = '<div class="recommended-meme-container flex flex-container flex-column flex-center">';
-    $output .= '<div class="recommended-meme-photo-container flex flex-container flex-center">';
-    $output .= '<img class="recommended-meme-photo drop-shadow" src="'.$content['image'].'">';
-    $output .= '</div>';
+require_once 'src/repository/MemeRepository.php';
 
-    $output .= '<div class="recommended-meme-title flex flex-container flex-center"><h4>';
-    $output .= $content['title'];
-    $output .= '</h4></div></div>';
+function RecommendedMeme(): string
+{
+    $memeRepository = new MemeRepository();
+    $bestWeek = $memeRepository->getBestMemes(3, 7);
+    $bestMonth = $memeRepository->getBestMemes(3, 30);
+
+    $output = '<aside class="recommended-memes-aside flex flex-column">';
+
+
+    $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
+    $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
+    $output .= '<div class="recommended-meme-title"><h2>Naj tygodnia</h2></div>';
+    foreach ($bestWeek as $meme) {
+        $output .= '<div class="recommended-meme-card"><img src="public/uploads/memes/';
+        $output .= $meme['photourl'];
+        $output .= '" alt="photo-url" />';
+        $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
+        $output .= '</div>';
+    }
+
+    $output .= '</div></div></div>';
+
+    $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
+    $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
+    $output .= '<div class="recommended-meme-title"><h2>Naj miesiąca</h2></div>';
+    foreach ($bestMonth as $meme) {
+        $output .= '<div class="recommended-meme-card"><img src="public/uploads/memes/';
+        $output .= $meme['photourl'];
+        $output .= '" alt="photo-url" />';
+        $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
+        $output .= '</div>';
+    }
+
+    $output .= '</div></div></div>';
+
+    $output .= '</aside>';
+
     return $output;
 }
