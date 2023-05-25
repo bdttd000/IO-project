6d3dd2acb1d6d061e852eaf13fc1b430af04ef50
MnStan <58117854+MnStan@users.noreply.github.com>
2023-05-25 23:36:36 +0200

    Fixed problem with gaps between elements in footer contents (#87)

--- public/views/contents/statuteContent.php
+++ public/views/contents/statuteContent.php
@@ -1,36 +1,36 @@
 <?php
 
 $statuteContent = '
-<h3>Regulamin strony MEMONKS</h3>
+<div class="terms-of-service-content">
+    <h3>1. Postanowienia ogólne</h3>
+    <p>a) Witamy na stronie MEMONKS! Przed korzystaniem z naszej platformy, prosimy o zapoznanie się z poniższymi zasadami i zobowiązujemy się do przestrzegania ich w celu utrzymania pozytywnej i przyjaznej społeczności. Poprzez korzystanie z naszej strony, akceptujesz niniejszy regulamin.</p>
+    <p>b) Termin "strona" odnosi się do serwisu MEMONKS, a "użytkownik" oznacza osobę, która przegląda, dodaje lub komentuje memy na stronie.</p>
+    <p>c) Strona MEMONKS zastrzega sobie prawo do zmiany niniejszego regulaminu w dowolnym momencie. Użytkownicy zostaną poinformowani o wszelkich zmianach w regulaminie.</p>
 
-<h3>1. Postanowienia ogólne</h3>
-<p>a) Witamy na stronie MEMONKS! Przed korzystaniem z naszej platformy, prosimy o zapoznanie się z poniższymi zasadami i zobowiązujemy się do przestrzegania ich w celu utrzymania pozytywnej i przyjaznej społeczności. Poprzez korzystanie z naszej strony, akceptujesz niniejszy regulamin.</p>
-<p>b) Termin "strona" odnosi się do serwisu MEMONKS, a "użytkownik" oznacza osobę, która przegląda, dodaje lub komentuje memy na stronie.</p>
-<p>c) Strona MEMONKS zastrzega sobie prawo do zmiany niniejszego regulaminu w dowolnym momencie. Użytkownicy zostaną poinformowani o wszelkich zmianach w regulaminie.</p>
+    <h3>2. Treści memów</h3>
+    <p>a) Użytkownicy są odpowiedzialni za treści memów, które publikują na stronie MEMONKS. Nie wolno dodawać treści, które są obraźliwe, rasistowskie, pornograficzne, nawołujące do przemocy lub w jakikolwiek sposób naruszające prawa innych osób.</p>
+    <p>b) Wszelkie prawa autorskie do memów należą do ich twórców. Użytkownicy nie mogą publikować memów, które naruszają prawa autorskie innych osób bez ich zgody.</p>
+    <p>c) Strona MEMONKS zastrzega sobie prawo do usuwania memów, które są niezgodne z niniejszym regulaminem lub które są zgłoszone jako naruszające prawa autorskie lub przepisy prawa.</p>
 
-<h3>2. Treści memów</h3>
-<p>a) Użytkownicy są odpowiedzialni za treści memów, które publikują na stronie MEMONKS. Nie wolno dodawać treści, które są obraźliwe, rasistowskie, pornograficzne, nawołujące do przemocy lub w jakikolwiek sposób naruszające prawa innych osób.</p>
-<p>b) Wszelkie prawa autorskie do memów należą do ich twórców. Użytkownicy nie mogą publikować memów, które naruszają prawa autorskie innych osób bez ich zgody.</p>
-<p>c) Strona MEMONKS zastrzega sobie prawo do usuwania memów, które są niezgodne z niniejszym regulaminem lub które są zgłoszone jako naruszające prawa autorskie lub przepisy prawa.</p>
+    <h3>3. Komentarze i zachowanie społeczności</h3>
+    <p>a) Zachęcamy użytkowników do komentowania memów i aktywnego udziału w społeczności MEMONKS. Jednakże, wszelkie komentarze powinny być konstruktywne i nieobraźliwe wobec innych użytkowników.</p>
+    <p>b) Nie tolerujemy treści, które są dyskryminacyjne, wulgarne, groźne lub naruszają czyjąś prywatność.</p>
+    <p>c) Użytkownicy są odpowiedzialni za swoje własne komentarze i zachowanie na stronie MEMONKS. Naruszenie tych zasad może skutkować usunięciem komentarza lub zablokowaniem dostępu do strony.</p>
 
-<h3>3. Komentarze i zachowanie społeczności</h3>
-<p>a) Zachęcamy użytkowników do komentowania memów i aktywnego udziału w społeczności MEMONKS. Jednakże, wszelkie komentarze powinny być konstruktywne i nieobraźliwe wobec innych użytkowników.</p>
-<p>b) Nie tolerujemy treści, które są dyskryminacyjne, wulgarne, groźne lub naruszają czyjąś prywatność.</p>
-<p>c) Użytkownicy są odpowiedzialni za swoje własne komentarze i zachowanie na stronie MEMONKS. Naruszenie tych zasad może skutkować usunięciem komentarza lub zablokowaniem dostępu do strony.</p>
+    <h3>4. Bezpieczeństwo i prywatność</h3>
+    <p>a) Strona MEMONKS podejmuje odpowiednie środki bezpieczeństwa, aby chronić dane użytkowników. Jednakże, użytkownicy powinni być świadomi, że transmisja danych przez internet wiąże się z pewnym ryzykiem i strona MEMONKS nie ponosi odpowiedzialności za ewentualne naruszenia bezpieczeństwa.</p>
+    <p>b) Użytkownicy nie powinni udostępniać prywatnych danych innych osób na stronie MEMONKS bez ich zgody.</p>
+    <p>c) Strona MEMONKS zastrzega sobie prawo do gromadzenia i wykorzystywania danych użytkowników zgodnie z naszą polityką prywatności.</p>
 
-<h3>4. Bezpieczeństwo i prywatność</h3>
-<p>a) Strona MEMONKS podejmuje odpowiednie środki bezpieczeństwa, aby chronić dane użytkowników. Jednakże, użytkownicy powinni być świadomi, że transmisja danych przez internet wiąże się z pewnym ryzykiem i strona MEMONKS nie ponosi odpowiedzialności za ewentualne naruszenia bezpieczeństwa.</p>
-<p>b) Użytkownicy nie powinni udostępniać prywatnych danych innych osób na stronie MEMONKS bez ich zgody.</p>
-<p>c) Strona MEMONKS zastrzega sobie prawo do gromadzenia i wykorzystywania danych użytkowników zgodnie z naszą polityką prywatności.</p>
+    <h3>5. Zgłaszanie naruszeń</h3>
+    <p>a) Użytkownicy mogą zgłaszać naruszenia regulaminu lub nieodpowiednie treści poprzez odpowiednie funkcje dostępne na stronie MEMONKS.</p>
+    <p>b) Strona MEMONKS podejmie odpowiednie działania wobec zgłoszonych naruszeń w miarę możliwości i zgodnie z naszymi możliwościami.</p>
 
-<h3>5. Zgłaszanie naruszeń</h3>
-<p>a) Użytkownicy mogą zgłaszać naruszenia regulaminu lub nieodpowiednie treści poprzez odpowiednie funkcje dostępne na stronie MEMONKS.</p>
-<p>b) Strona MEMONKS podejmie odpowiednie działania wobec zgłoszonych naruszeń w miarę możliwości i zgodnie z naszymi możliwościami.</p>
+    <h3>6. Postanowienia końcowe</h3>
+    <p>a) Niniejszy regulamin stanowi umowę między użytkownikiem a stroną MEMONKS i reguluje korzystanie z naszej strony.</p>
+    <p>b) Jeśli użytkownik nie zgadza się z jakimkolwiek postanowieniem niniejszego regulaminu, powinien powstrzymać się od korzystania z naszej strony.</p>
+    <p>c) W przypadku naruszenia niniejszego regulaminu, strona MEMONKS może podjąć odpowiednie działania, włączając w to zablokowanie dostępu do strony lub zgłoszenie sprawy odpowiednim organom ścigania.</p>
 
-<h3>6. Postanowienia końcowe</h3>
-<p>a) Niniejszy regulamin stanowi umowę między użytkownikiem a stroną MEMONKS i reguluje korzystanie z naszej strony.</p>
-<p>b) Jeśli użytkownik nie zgadza się z jakimkolwiek postanowieniem niniejszego regulaminu, powinien powstrzymać się od korzystania z naszej strony.</p>
-<p>c) W przypadku naruszenia niniejszego regulaminu, strona MEMONKS może podjąć odpowiednie działania, włączając w to zablokowanie dostępu do strony lub zgłoszenie sprawy odpowiednim organom ścigania.</p>
-
-<p>Dziękujemy za korzystanie z naszej strony MEMONKS i życzymy przyjemnego czasu spędzonego na przeglądaniu memów oraz interakcji w społeczności!</p>
+    <p>Dziękujemy za korzystanie z naszej strony MEMONKS i życzymy przyjemnego czasu spędzonego na przeglądaniu memów oraz interakcji w społeczności!</p>
+</div>
 ';
