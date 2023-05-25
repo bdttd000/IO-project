641cbbf7190104f18bb50c58a982078c90fd29ba
MnStan <mmaks97@gmail.com>
2023-05-25 23:33:07 +0200

    Fixed problem with gaps between elements in footer contents

--- public/views/contents/privacyPolicyContent.php
+++ public/views/contents/privacyPolicyContent.php
@@ -1,31 +1,33 @@
 <?php
 
 $privacyPolicyContent = '
-<p>Niniejsza polityka prywatności określa, w jaki sposób strona MEMONKS gromadzi, wykorzystuje i chroni informacje podane przez użytkowników podczas korzystania z naszej strony.</p>
+<div class="privacy-policy-content">
+    <p>Niniejsza polityka prywatności określa, w jaki sposób strona MEMONKS gromadzi, wykorzystuje i chroni informacje podane przez użytkowników podczas korzystania z naszej strony.</p>
 
-<h3>Gromadzenie informacji</h3>
-<p>Podczas korzystania z naszej strony, możemy gromadzić pewne informacje, takie jak:</p>
-<ul class="privacy-policy">
-    <li>Informacje osobiste: Nick, adres e-mail (jeśli użytkownik dobrowolnie poda te dane).</li>
-    <li>Dane techniczne: Adres IP, dane przeglądarki, informacje o systemie operacyjnym.</li>
-    <li>Cookies: Możemy używać plików cookie w celu personalizacji zawartości i ułatwienia korzystania z naszej strony.</li>
-</ul>
+    <h3>Gromadzenie informacji</h3>
+    <p>Podczas korzystania z naszej strony, możemy gromadzić pewne informacje, takie jak:</p>
+    <ul class="privacy-policy">
+        <li>Informacje osobiste: Nick, adres e-mail (jeśli użytkownik dobrowolnie poda te dane).</li>
+        <li>Dane techniczne: Adres IP, dane przeglądarki, informacje o systemie operacyjnym.</li>
+        <li>Cookies: Możemy używać plików cookie w celu personalizacji zawartości i ułatwienia korzystania z naszej strony.</li>
+    </ul>
 
-<h3>Wykorzystanie informacji</h3>
-<p>Informacje gromadzone przez stronę MEMONKS mogą być wykorzystywane w następujący sposób:</p>
-<ul class="privacy-policy">
-    <li>Personalizacja doświadczenia użytkownika na stronie.</li>
-    <li>Wysyłanie wiadomości e-mail dotyczących aktualizacji, promocji lub innych powiązanych informacji.</li>
-    <li>Poprawa naszej strony i jej funkcjonalności.</li>
-    <li>Zapewnienie bezpieczeństwa i ochrony naszej strony oraz użytkowników.</li>
-</ul>
+    <h3>Wykorzystanie informacji</h3>
+    <p>Informacje gromadzone przez stronę MEMONKS mogą być wykorzystywane w następujący sposób:</p>
+    <ul class="privacy-policy">
+        <li>Personalizacja doświadczenia użytkownika na stronie.</li>
+        <li>Wysyłanie wiadomości e-mail dotyczących aktualizacji, promocji lub innych powiązanych informacji.</li>
+        <li>Poprawa naszej strony i jej funkcjonalności.</li>
+        <li>Zapewnienie bezpieczeństwa i ochrony naszej strony oraz użytkowników.</li>
+    </ul>
 
-<h3>Dostęp osób trzecich</h3>
-<p>Nie udostępniamy informacji osobowych użytkowników strony MEMONKS osobom trzecim bez ich zgody, chyba że wymaga tego prawo lub jest to niezbędne do zapewnienia działania i bezpieczeństwa naszej strony.</p>
+    <h3>Dostęp osób trzecich</h3>
+    <p>Nie udostępniamy informacji osobowych użytkowników strony MEMONKS osobom trzecim bez ich zgody, chyba że wymaga tego prawo lub jest to niezbędne do zapewnienia działania i bezpieczeństwa naszej strony.</p>
 
-<h3>Ochrona danych</h3>
-<p>Stosujemy odpowiednie środki techniczne i organizacyjne, aby chronić informacje użytkowników przed nieautoryzowanym dostępem, utratą lub zmianą.</p>
+    <h3>Ochrona danych</h3>
+    <p>Stosujemy odpowiednie środki techniczne i organizacyjne, aby chronić informacje użytkowników przed nieautoryzowanym dostępem, utratą lub zmianą.</p>
 
-<h3>Linki do innych stron</h3>
-<p>Nasza strona może zawierać linki do innych stron. Nie ponosimy odpowiedzialności za polityki prywatności lub treści tych stron.</p>
+    <h3>Linki do innych stron</h3>
+    <p>Nasza strona może zawierać linki do innych stron. Nie ponosimy odpowiedzialności za polityki prywatności lub treści tych stron.</p>
+</div>
 ';
