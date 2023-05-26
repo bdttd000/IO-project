f1647acbad7be9795457cfe7da0b0964e5d4399e
Jakub Wartak <kuba428@op.pl>
2023-05-26 18:18:21 +0200

    memesolo component

---
+++ public/views/components/memeSolo.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "public/views/components/memeLikes.php";
+require_once "public/views/components/buttonRedirect.php";
+require_once "public/views/components/memeComment.php";
+require_once "src/models/Meme.php";
+require_once "src/repository/UserRepository.php";
+
+function Meme(Meme $meme): string
+{
+    $userRepository = new UserRepository();
+    $user = $userRepository->getUserById($meme->getUserID());
+
+    $buttonArray = [
+        'link' => 'meme?memeid=' . $meme->getMemeID(),
+        'value' => 'Zobacz więcej / Dodaj komentarz'
+    ];
+
+    $output = '
+    <div class="meme">
+
+    <div class="meme-content">
+    <div class="meme-title-and-likes">';
+    $output .= '<div class="meme-title drop-shadow"><h3 class="meme-title-content">';
+    $output .= $meme->getTitle();
+    $output .= '</h3></div>';
+    $output .= MemeLikes($meme->getLikes());
+    $output .= '</div>';
+
+    $output .= '<div class="meme-user-date-favorite">';
+    $output .= '<div class="meme-user-info">';
+
+    $output .= '<img src="public/uploads/avatars/';
+    $output .= ($user->getAvatarUrl()) ? $user->getAvatarUrl() : 'unknown.png';
+    $output .= '" class="meme-user-avatar">';
+
+    $output .= '<div class="meme-user-name"><h3>';
+    $output .= $user->getNickname();
+    $output .= '</h3></div>';
+    $output .= '</div>';
+    $output .= '<div class="meme-date"><h3>';
+    $output .= $meme->getCreationDate();
+    $output .= '</h3></div>';
+    $output .= '<p id="meme-favorite-button">&#9825</p>';
+    $output .= '</div>';
+
+    $output .= '<div class="meme-photo-container">';
+    $output .= '<img class="meme-photo drop-shadow" src="public/uploads/memes/' . $meme->getPhotoUrl() . '">';
+    $output .= '</div>';
+
+    $output .= '<div>';
+
+    $counter = 1;
+    foreach ($meme->getComments() as $comment) {
+        $output .= MemeComment($comment, (bool) ($counter % 2));
+        $counter++;
+    }
+    $output .= '</div>';
+
+    //TODO: ADD FORM TO ADD COMMENT AND FAKE IT WITH JS
+
+    $output .= '</div></div>';
+
+    return $output;
+}
--- public/views/components/recommendedMeme.php
+++ public/views/components/recommendedMeme.php
@@ -2,40 +2,43 @@
 
 require_once 'src/repository/MemeRepository.php';
 
-function RecommendedMeme(): string
+function RecommendedMeme(int $numberOfWeekMemes = 3, int $numberOfMonthMemes = 3): string
 {
     $memeRepository = new MemeRepository();
-    $bestWeek = $memeRepository->getBestMemes(3, 7);
-    $bestMonth = $memeRepository->getBestMemes(3, 30);
+    $bestWeek = $memeRepository->getBestMemes($numberOfWeekMemes, 7);
+    $bestMonth = $memeRepository->getBestMemes($numberOfMonthMemes, 30);
 
     $output = '<aside class="recommended-memes-aside flex flex-column">';
 
+    if ($numberOfWeekMemes > 0) {
+        $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
+        $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
+        $output .= '<div class="recommended-meme-title"><h2>Top tygodnia</h2></div>';
+        foreach ($bestWeek as $meme) {
+            $output .= '<a href="meme?memeid=' . $meme['memeid'] . '" class="recommended-meme-card"><img src="public/uploads/memes/';
+            $output .= $meme['photourl'];
+            $output .= '" alt="photo-url" />';
+            $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
+            $output .= '</a>';
+        }
 
-    $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
-    $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
-    $output .= '<div class="recommended-meme-title"><h2>Naj tygodnia</h2></div>';
-    foreach ($bestWeek as $meme) {
-        $output .= '<div class="recommended-meme-card"><img src="public/uploads/memes/';
-        $output .= $meme['photourl'];
-        $output .= '" alt="photo-url" />';
-        $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
-        $output .= '</div>';
+        $output .= '</div></div></div>';
     }
 
-    $output .= '</div></div></div>';
+    if ($numberOfMonthMemes > 0) {
+        $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
+        $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
+        $output .= '<div class="recommended-meme-title"><h2>Top miesiąca</h2></div>';
+        foreach ($bestMonth as $meme) {
+            $output .= '<a href="meme?memeid=' . $meme['memeid'] . '" class="recommended-meme-card"><img src="public/uploads/memes/';
+            $output .= $meme['photourl'];
+            $output .= '" alt="photo-url" />';
+            $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
+            $output .= '</a>';
+        }
 
-    $output .= '<div class="recommended-meme-sticky"><div class="recommended-memes flex flex-center flex-column">';
-    $output .= '<div class="recommended-meme-box flex flex-center flex-column drop-shadow">';
-    $output .= '<div class="recommended-meme-title"><h2>Naj miesiąca</h2></div>';
-    foreach ($bestMonth as $meme) {
-        $output .= '<div class="recommended-meme-card"><img src="public/uploads/memes/';
-        $output .= $meme['photourl'];
-        $output .= '" alt="photo-url" />';
-        $output .= '<div class="flex flex-center">' . $meme['title'] . '</div>';
-        $output .= '</div>';
+        $output .= '</div></div></div>';
     }
-
-    $output .= '</div></div></div>';
 
     $output .= '</aside>';
 
