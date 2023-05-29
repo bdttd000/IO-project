2a49faeda2bfa43970e6001bebd874732ee08c79
Jakub Wartak <kuba428@op.pl>
2023-05-29 21:31:21 +0200

    changed memeLikes component

--- public/views/components/memeLikes.php
+++ public/views/components/memeLikes.php
@@ -5,26 +5,51 @@ function MemeLikes(int $likes, int $memeid): string
     $SessionController = new SessionController();
     $userInfo = $SessionController->unserializeUser();
 
-    $output = '<div class="meme-likes-and-buttons drop-shadow">
-    <div data-likes-action="like" class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-minus like-button" href="';
+    $output = '<div class="meme-likes-and-buttons drop-shadow">';
 
     if (isset($userInfo) && $userInfo->getUserID()) {
-        $output .= 'addDislike?memeid=' . $memeid . '&userid=' . $userInfo->getUserID();
+        $output .= '
+        <div 
+            data-likes-action="addDislike" 
+            data-meme-id="' . $memeid . '" 
+            class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-minus like-button"
+        >
+            &#8722;
+        </div>
+        ';
     } else {
-        $output .= 'login';
+        $output .= '
+        <a 
+        href="login"
+        class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-minus like-button"
+        >&#8722</a>
+        ';
     }
 
-    $output .= '">&#8722;</div><div class="meme-likes"> <h3 class="meme-likes-number">' . $likes . '</h3></div> 
-    <div data-likes-action="dislike" class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-plus like-button" href="';
+    $output .= '<div class="meme-likes"><h3 data-meme-id=' . $memeid . ' class="meme-likes-number">' . $likes . '</h3></div>';
 
     if (isset($userInfo) && $userInfo->getUserID()) {
-        $output .= 'addLike?memeid=' . $memeid . '&userid=' . $userInfo->getUserID();
+        $output .= '
+        <div 
+            data-likes-action="addLike"
+            data-meme-id="' . $memeid . '" 
+            class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-plus like-button"
+        >
+            &#43;
+        </div>
+        ';
     } else {
-        $output .= 'login';
+        $output .= '
+        <a 
+            href="login"
+            class="flex flex-center drop-shadow-animate meme-likes-symbol meme-likes-symbol-plus like-button"
+        >
+            &#8722
+        </a>
+        ';
     }
 
-    $output .= '">&#43;</div> 
-    </div>';
+    $output .= '</div>';
 
     return $output;
 }
