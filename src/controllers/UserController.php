3fd783bd34f5882df41f9de6929806a2e41f2228
Jakub Wartak <kuba428@op.pl>
2023-06-01 14:45:03 +0200

    added edit profile logic

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,12 +1,16 @@
 <?php
 
 require_once 'AppController.php';
-// require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../models/User.php';
 require_once __DIR__ . '/../repository/UserRepository.php';
-// require_once 'SessionController.php';
+require_once 'SessionController.php';
 
 class UserController extends AppController
 {
+    const MAX_FILE_SIZE = 1024 * 1024;
+    const SUPPORTED_TYPES = ['image/png', 'image/jpeg'];
+    const UPLOAD_DIRECTORY = '/../public/uploads/avatars/';
+    private static $messages = [];
     private $userRepository;
 
     public function __construct()
@@ -34,5 +38,61 @@ class UserController extends AppController
     public function editProfile()
     {
         $this->render('editProfile');
+    }
+
+    public function editProfileForm()
+    {
+        if (!$this->isPost()) {
+            return $this->redirectToHome();
+        }
+
+        $sessionController = new SessionController();
+        $userInfo = $sessionController->unserializeUser();
+
+        if (is_uploaded_file($_FILES['avatar']['tmp_name']) && $this->validateFile($_FILES['avatar'])) {
+            $newUrl = $this->userRepository->generateID() . '.' . pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+
+            move_uploaded_file(
+                $_FILES['avatar']['tmp_name'],
+                dirname(__DIR__) . self::UPLOAD_DIRECTORY . $newUrl
+            );
+
+            if ($userInfo->getAvatarUrl() != '') {
+                unlink(dirname(__DIR__) . self::UPLOAD_DIRECTORY . $userInfo->getAvatarUrl());
+            }
+
+            $userInfo->setAvatarUrl($newUrl);
+        } else {
+            $newUrl = $userInfo->getAvatarUrl();
+        }
+
+        if ($_POST['description'] !== $userInfo->getDescription()) {
+            $userInfo->setDescription($_POST['description']);
+        }
+
+        $_SESSION["user_info"] = serialize($userInfo);
+
+        $this->userRepository->editUser(
+            $newUrl,
+            $_POST['description'],
+            $userInfo->getUserID()
+        );
+
+        $this->profile('userid=' . $userInfo->getUserID());
+    }
+
+    private function validateFile(array $file): bool
+    {
+        if ($file['size'] > self::MAX_FILE_SIZE) {
+            array_push(self::$messages, 'Plik jest za duży');
+            return false;
+        }
+
+        if (!isset($file['type']) && !in_array($file['type'], self::SUPPORTED_TYPES)) {
+            array_push(self::$messages, 'Rozszerzenie pliku jest niedozwolone');
+            return false;
+        }
+
+        return true;
     }
 }
